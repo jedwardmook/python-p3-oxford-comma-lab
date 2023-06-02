2bb7c4c13a907d3a64819ec75e9fb96cc33da719
JedwardMook <jedwardmook@yahoo.com>
2023-06-01 22:21:51 -0500

    solutions

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if (len(items) == 1):
+        return "".join(items)
+    elif (len(items) == 2):
+        return " and ".join(items)
+    else:
+        items_end = items.pop()
+        and_str = ", and "
+        items_and = ", ".join(items) + and_str + items_end
+        return items_and
+        
